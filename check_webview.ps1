9ba95bbd28694239356288ffbdef3fe10b01ef2d
unknown <wanganxin@sunrise-ai.com>
2026-09-23 17:44:02 +0800

    chore: clean up temp files and add .gitignore

--- check_webview.ps1
+++
@@ -1,8 +0,0 @@
-$pids = @()
-Get-CimInstance Win32_Process | ForEach-Object {
-    if ($_.CommandLine -match 'msedgewebview|cc-switch|codex') {
-        $cmd = $_.CommandLine
-        if ($cmd.Length -gt 200) { $cmd = $cmd.Substring(0, 200) }
-        Write-Output "PID: $($_.ProcessId) | Name: $($_.Name) | Cmd: $cmd"
-    }
-}
